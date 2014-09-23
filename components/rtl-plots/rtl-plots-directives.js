a07b3e848d56ad12b56b754133cad9e4fb2ee1bf
Rob Cannon <rob.cannon@axiosengineering.com>
2014-09-23 14:14:43 -0400

    Added multiple types for plots. Added Narrowband view.

--- components/rtl-plots/rtl-plots-directives.js
+++ components/rtl-plots/rtl-plots-directives.js
@@ -2,14 +2,62 @@
  * Created by Rob Cannon on 9/22/14.
  */
 angular.module('rtl-plots', ['SubscriptionSocketService'])
-    .directive('rtlPlot', ['SubscriptionSocket',
-      function(SubscriptionSocket){
+    .service('plotDataConverter', function(){
+      /*
+       Create a map to convert the standard REDHAWK BulkIO Formats
+       into Javascript equivalents.
+       ----
+       byte      = 8-bit signed
+       char      = 8-bit unsigned
+       octet     = 8-bit The signed-ness is undefined
+       short     = 16-bit signed integer
+       ushort    = 16-bit unsigned integer
+       long      = 32-bit signed integer
+       ulong     = 32-bit unsigned integer
+       longlong  = 64-bit signed integer
+       ulonglong = 64-bit unsigned integer
+       float     = 32-bit floating point
+       double    = 64-bit floating point
+       ----
+       */
+      var conversionMap = {
+        byte: Int8Array,
+        char: Uint8Array,
+        octet: Uint8Array,
+        ushort: Uint16Array,
+        short: Int16Array,
+        long: Int32Array,
+        ulong: Uint32Array,
+        longlong: undefined, //This should be 64-bit
+        ulonglong: undefined, //This should be 64-bit
+        float: Float32Array,
+        double: Float64Array
+      };
+      var defaultConversion = Float32Array;
+
+      return function(type) {
+        var fn = conversionMap[type];
+
+        if(type == 'octet')
+          console.log("Plot::DataConverter::WARNING - Data type is 'octet' assuming unsigned.");
+
+        if(!fn) {
+          console.log("Plot::DataConverter::WARNING - Data type is '"+type+"' using default.");
+          fn = defaultConversion;
+        }
+
+        return function(data) { return new fn(data); };
+      };
+    })
+    .directive('rtlPlot', ['SubscriptionSocket', 'plotDataConverter',
+      function(SubscriptionSocket, plotDataConverter){
         return {
           restrict: 'E',
           scope: {
             width: '@',
             height: '@',
-            url: '@'
+            url: '@',
+            type: '@'
           },
           template: '<div style="width: {{width}}; height: {{height}};" id="plot" ></div>',
           link: function (scope, element, attrs) {
@@ -29,10 +77,9 @@ angular.module('rtl-plots', ['SubscriptionSocketService'])
               format: 'SF'
             };
             scope.plotSettings = angular.copy(defaultSettings);
+            scope.plotSettings = angular.extend(scope.plotSettings, scope.settings);
 
             var createPlot = function(format, settings) {
-              console.log(element);
-
               plot = new sigplot.Plot(element[0].firstChild, {
                 autohide_panbars: true,
                 autox: 3,
@@ -82,7 +129,31 @@ angular.module('rtl-plots', ['SubscriptionSocketService'])
               }
 
               if (!plot) {
-                createPlot("SD", scope.plotSettings);
+                var mode = undefined;
+                switch (data.mode) {
+                  case 0:
+                    mode = "S";
+                    break;
+                  case 1:
+                    mode = "C";
+                    break;
+                  default:
+                }
+
+                if (mode) {
+                  switch (scope.type) {
+                    case "float":
+                      createPlot(mode + "F", scope.plotSettings);
+                      console.log("Create plots with format " + mode + "F");
+                      break;
+                    case "double":
+                      createPlot(mode + "D", scope.plotSettings);
+                      console.log("Create plots with format " + mode + "D");
+                      break;
+                    default:
+                  }
+                  isDirty = true;
+                }
               }
 
               if(isDirty) {
@@ -94,9 +165,10 @@ angular.module('rtl-plots', ['SubscriptionSocketService'])
               updatePlotSettings(sri);
             };
 
+            var dataConverter = plotDataConverter(scope.type);
             var lastDataSize = 1000;
             var on_data = function(data) {
-              var array = new Float64Array(data);
+              var array = dataConverter(data);
 
               lastDataSize = array.length;
 
@@ -110,7 +182,7 @@ angular.module('rtl-plots', ['SubscriptionSocketService'])
               socket.addJSONListener(on_sri);
 
             socket.connect(scope.url, function(){
-              console.log("Connected to Survey Wideband");
+              console.log("Connected to Plot");
             });
 
             scope.$on("$destroy", function(){
