6c4101c6705d949a8247c5117536d8519eb57c60
Mark Leone <mark.leone@axiosengineering.com>
2014-10-02 19:07:31 -0400

    Add raster plot directive

--- components/rtl-plots/rtl-plots-directives.js
+++ components/rtl-plots/rtl-plots-directives.js
@@ -366,4 +366,313 @@ angular.module('rtl-plots', ['SubscriptionSocketService'])
         };
       }
     ])
+  .directive('rtlRaster', ['SubscriptionSocket', 'plotDataConverter',
+    function(SubscriptionSocket, plotDataConverter){
+      return {
+        restrict: 'E',
+        scope: {
+          width: '@',
+          height: '@',
+          url: '@',
+          type: '@',
+          doTune: '&'
+        },
+        template: '<div style="width: {{width}}; height: {{height}};" id="plot" ></div>',
+        link: function (scope, element, attrs) {
+          var socket = SubscriptionSocket.createNew();
+
+          var plot, layer, accordion;
+
+          var bw = 100000;
+
+          var spectrumBw = 2e6;
+
+          var tunedFreq;
+
+          var defaultSettings = {
+            xdelta:10.25390625,
+            xstart: -1,//ensure change is detected with first SRI
+            xunits: 3,
+            ydelta : 0.09752380952380953,
+            ystart: 0,
+            yunits: 3,
+            subsize: 32768,
+            size: 32768,
+            format: 'SF' };
+          scope.plotSettings = angular.copy(defaultSettings);
+
+          var createPlot = function(format, settings) {
+            plot = new sigplot.Plot(element[0].firstChild, {
+              all: true,
+              expand: true,
+              autol: 100,
+              autox: 3,
+              autohide_panbars: true,
+              cmode: "D2", //20Log
+//              colors: {bg: "rgba(255,255,255,1)", fg: "rgba(0,0,0,1)"}
+            });
+            if (scope.url.indexOf('psd/wideband') >= 0) {
+              plot.addListener('mdown', plotMDownListener);
+              plot.addListener('mup', plotMupListener);
+            }
+
+            layer = plot.overlay_pipe(angular.extend(settings, {type: 2000, 'format': format, pipe: true, pipesize: 1024 * 1024 * 5, yunits: 28}));
+            accordion = new sigplot.AccordionPlugin({
+              draw_center_line: true,
+              shade_area: false,
+              draw_edge_lines: false,
+              direction: "vertical",
+              edge_line_style: {strokeStyle: "#FF0000"}
+            });
+
+            plot.add_plugin(accordion, layer + 1);
+            //modifyWarpboxBehavior(plot);
+          };
+
+          var lastMouseDown = {
+            x: undefined,
+            y: undefined
+          };
+
+          var plotMDownListener = function(event) {
+            lastMouseDown.x = event.x;
+            lastMouseDown.y = event.y;
+          };
+
+          var clickTolerance = 200;
+          var plotMupListener = function(event) {
+            if (Math.abs(event.x - lastMouseDown.x) <= clickTolerance && event.which === 1) {
+              if (inPlotBounds(event.x, event.y)) {
+                console.log("Tuned to " + event.x / 1000 + " KHz");
+                scope.doTune({cf: event.x});
+              }
+            } else if (Math.abs(event.x - lastMouseDown.x) >= clickTolerance && event.which == 3) {
+              //disable drag tuning until warpbox can be drawn
+              //dragTune(event);
+            }
+          };
+
+          var dragTune = function(event) {
+            if (lastMouseDown.x && lastMouseDown.y) {
+              var rect = {
+                x1: lastMouseDown.x,
+                x2: event.x,
+                y1: lastMouseDown.y,
+                y2: event.y
+              };
+              lastMouseDown = {x:undefined, y: undefined};
+              scope.doTune({cf:(rect.x1 + rect.x2) / 2});
+              console.log("Tuned to sub-band from " + rect.x1 / 1000  + " KHz to " + rect.x2 / 1000 + " KHz");
+            }
+          };
+
+          var inPlotBounds = function(x, y) {
+            var zoomStack = plot._Mx.stk[plot._Mx.stk.length - 1];
+            var xmin = zoomStack.xmin;
+            var xmax = zoomStack.xmax;
+            var ymin = zoomStack.ymin;
+            var ymax = zoomStack.ymax;
+            //if clicking on x position > xmax, x will be set to xmax. Same for y values
+            if (x >= xmax || x <= xmin || y >= ymax || y <= ymin) {
+              return false;
+            }
+            return true;
+          }
+
+          var showHighlight = function (cf) {
+            tunedFreq = cf;//used in wideband plot to determine min/max x values in mouse listeners
+            if (scope.url.indexOf('psd/narrowband') >= 0) {
+              cf = 0;
+              bw = 100e3//TODO get value from TuneFilterDecimate component
+            }
+            if (plot && cf !== undefined) {
+              if (scope.url.indexOf('psd/wideband') >= 0) {
+                accordion.set_center(cf);
+                accordion.set_width(bw);
+              }
+            }
+
+          };
+
+          var reloadSri;
+          var lastCf;
+          var lastXStart = -1;
+
+          //var ignoreNewXStart;
+
+          var mode = undefined;
+
+          var updatePlotSettings = function(data) {
+            var isDirty = false;
+            var cf = data.keywords.CHAN_RF;
+            var xstart = data.xstart;
+            if (Math.abs(lastXStart - xstart) >= 0) {
+              console.log(scope.url + ' xstart: ' + xstart);
+              lastXStart = xstart;
+              showHighlight(cf);
+              isDirty = true;
+            }
+            angular.forEach(data, function(item, key){
+              if (angular.isDefined(scope.plotSettings[key]) && !angular.equals(scope.plotSettings[key], item)) {
+                isDirty = true;
+                console.log(scope.url + ": Plot settings change "+key+": "+scope.plotSettings[key]+" -> "+item);
+                scope.plotSettings[key] = item;
+              }
+            });
+
+            scope.plotSettings['size'] = lastDataSize * scope.plotSettings['xdelta'];
+
+            if (!plot) {
+              switch (data.mode) {
+                case 0:
+                  mode = "S";
+                  break;
+                case 1:
+                  mode = "C";
+                  break;
+                default:
+              }
+
+              if (mode) {
+                switch (scope.type) {
+                  case "float":
+                    createPlot(mode + "F", defaultSettings);
+                    console.log("Create plots with format " + mode + "F");
+                    break;
+                  case "double":
+                    createPlot(mode + "D", defaultSettings);
+                    console.log("Create plots with format " + mode + "D");
+                    break;
+                  default:
+                }
+                showHighlight(cf);
+                isDirty = true;
+              }
+            }
+
+            if(isDirty) {
+              reloadSri = true;
+            }
+          };
+
+          var on_sri = function(sri) {
+            updatePlotSettings(sri);
+          };
+
+          var dataConverter = plotDataConverter(scope.type);
+          var lastDataSize;
+
+          var reported = 0;
+
+          var on_data = function(data) {
+            var bps;
+            switch (scope.type) {
+              case 'double':
+                bps = 2;
+                break;
+              case 'float':
+                bps = 4;
+                break;
+              default:
+                return;
+            };
+
+            var bpe;
+            switch (mode) {
+              case 'S':
+                bpe = bps;
+                break;
+              case 'C':
+                bpe = bps * 2;
+                break;
+              default:
+                return;
+            }
+
+            var frameSize = scope.plotSettings.subsize * bpe;
+            var numFrames = Math.floor(data.byteLength / frameSize );
+            if (reported <= 5) {
+              console.log(scope.url + ': subsze: ' + scope.plotSettings.subsize + ' numBytes: ' + data.byteLength + ' numFrames: ' + numFrames + ' frameSize: ' + frameSize);
+              reported++;
+            }
+            //workaround: take ony first frame, as loading frames seriatum seems to not work
+            //back-end will be modified to send only one frame
+            for (var i = 0; i < frameSize /** (numFrames - 1)*/; i+= frameSize) {
+              data = data.slice(i, i + frameSize);
+              var array = dataConverter(data);
+              lastDataSize = array.length;
+              if (plot) {
+                if (reported <= 5) {
+                  console.log('plotting data from index ' + i + " to " + (i+frameSize));
+                }
+                reloadPlot(array);
+              }
+            }
+          };
+
+          var reloadPlot = function(data) {
+            if (reloadSri) {
+              plot.reload(layer, data, scope.plotSettings);
+              plot.refresh();
+              plot._Gx.ylab = 27; //this is a hack, but the only way I can get sigplot to take the value
+              reloadSri = false;
+            } else {
+              plot.reload(layer, data);
+              plot._Gx.ylab = 27; //this is a hack, but the only way I can get sigplot to take the value
+            }
+          };
+
+          var modifyWarpboxBehavior = function(plot) {
+            plot._Mx.onmouseup = (function(Mx) {
+              alert('yay warpbox');
+              return function(event) {
+                if (Mx.warpbox) {
+                  mx.onWidgetLayer(Mx, function() {
+                    mx.erase_window(Mx);
+                  });
+
+                  var old_warpbox = Mx.warpbox;
+                  Mx.warpbox = undefined;
+
+                  if (event.which === 1 || event.which === 3) {
+                    if (old_warpbox.func) {
+                      var xo = old_warpbox.xo;
+                      var yo = old_warpbox.yo;
+                      var xl = old_warpbox.xl;
+                      var yl = old_warpbox.yl;
+
+                      if (old_warpbox.mode === "vertical") {
+                        xo = Mx.l;
+                        xl = Mx.r;
+                      } else if (old_warpbox.mode === "horizontal") {
+                        yo = Mx.t;
+                        yl = Mx.b;
+                      } // else "box"
+                      old_warpbox.func(event, xo, yo, xl, yl, old_warpbox.style.return_value);
+                    }
+                  }
+
+                }
+                mx.widget_callback(Mx, event);
+              };
+            })(Mx);
+          };
+
+          if(on_data)
+            socket.addBinaryListener(on_data);
+          if(on_sri)
+            socket.addJSONListener(on_sri);
+
+          socket.connect(scope.url, function(){
+            console.log("Connected to Plot");
+          });
+
+          scope.$on("$destroy", function(){
+            socket.close();
+          })
+
+        }
+      };
+    }
+  ])
 ;
